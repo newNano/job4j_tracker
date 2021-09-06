6a778abfdb4bbd0e1daf08bd90798389894e4d92
11_pochta@bk.ru <11_pochta@bk.ru>
2021-09-06 16:13:30 +0600

    4. Переопределение [#174890 #199226] Заменил верхнюю фигурную скобку

--- src/main/java/ru/job4j/inheritance2/JSONReport.java
+++ src/main/java/ru/job4j/inheritance2/JSONReport.java
@@ -3,7 +3,7 @@ package ru.job4j.inheritance2;
 public class JSONReport extends TextReport {
     @Override
     public String generate(String name, String body) {
-        return "}" + System.lineSeparator()
+        return "{" + System.lineSeparator()
                + "\t\"" + name + "\" : \"" + name + "\" ," + System.lineSeparator()
                + "\t\"" + body + "\" : \"" + body + "\"" + System.lineSeparator()
                 + "}";
