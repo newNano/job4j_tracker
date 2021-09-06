eeebff157926da6751b1b679721076c891dfac1f
11_pochta@bk.ru <11_pochta@bk.ru>
2021-09-06 17:18:08 +0600

    2. Модель данных. [#174683 #199543] Убрал номер с имени объекта класса Student. Для переноса строки вместо \n прописал  System.lineSeparator()

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -2,12 +2,12 @@ package ru.job4j.pojo;
 
 public class College {
     public static void main(String[] args) {
-        Student student1 = new Student();
-        student1.setName("Виктор");
-        student1.setGroup("ИВТ 1-21");
-        student1.setEntered("2021-07-11");
-        System.out.println("Имя: " + student1.getName() + "\n"
-                            + "Группа: " + student1.getGroup() + "\n"
-                            + "Дата поступления: " + student1.getEntered());
+        Student student = new Student();
+        student.setName("Виктор");
+        student.setGroup("ИВТ 1-21");
+        student.setEntered("2021-07-11");
+        System.out.println("Имя: " + student.getName() + System.lineSeparator()
+                            + "Группа: " + student.getGroup() + System.lineSeparator()
+                            + "Дата поступления: " + student.getEntered());
     }
 }
