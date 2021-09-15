1b454dcc3889b0494e0a937fb647f544bade8880
11_pochta@bk.ru <11_pochta@bk.ru>
2021-09-15 20:41:54 +0600

    9.2. Тесты вывода на консоль [#33585 #203053]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -77,9 +77,9 @@ public class StartUITest {
     public void whenFindAllItems() {
         Output output = new StubOutput();
         Tracker tracker = new Tracker();
-        tracker.add(new Item("item-1"));
-        tracker.add(new Item("item-2"));
-        tracker.add(new Item("item-3"));
+        Item item1 = tracker.add(new Item("item-1"));
+        Item item2 = tracker.add(new Item("item-2"));
+        Item item3 = tracker.add(new Item("item-3"));
         Input in = new StubInput(
                 new String[]{"0", "1"}
         );
@@ -92,9 +92,9 @@ public class StartUITest {
                                                 + "0. Show all items" + System.lineSeparator()
                                                 + "1. Exit" + System.lineSeparator()
                                                 + "=== Show all items ====" + System.lineSeparator()
-                                                + "Item{id=" + tracker.findAll()[0].getId() + ", name='" + tracker.findAll()[0].getName() + "\'}" + System.lineSeparator()
-                                                + "Item{id=" + tracker.findAll()[1].getId() + ", name='" + tracker.findAll()[1].getName() + "\'}" + System.lineSeparator()
-                                                + "Item{id=" + tracker.findAll()[2].getId() + ", name='" + tracker.findAll()[2].getName() + "\'}" + System.lineSeparator()
+                                                + "Item{id=" + item1.getId() + ", name='" + item1.getName() + "\'}" + System.lineSeparator()
+                                                + "Item{id=" + item2.getId() + ", name='" + item2.getName() + "\'}" + System.lineSeparator()
+                                                + "Item{id=" + item3.getId() + ", name='" + item3.getName() + "\'}" + System.lineSeparator()
                                                 + "Menu." + System.lineSeparator()
                                                 + "0. Show all items" + System.lineSeparator()
                                                 + "1. Exit" + System.lineSeparator()
@@ -105,11 +105,11 @@ public class StartUITest {
     public void whenFindItemsByName() {
         Output output = new StubOutput();
         Tracker tracker = new Tracker();
-        tracker.add(new Item("itemA"));
-        tracker.add(new Item("itemB"));
-        tracker.add(new Item("itemC"));
-        tracker.add(new Item("itemA"));
-        tracker.add(new Item("itemC"));
+        Item item1 = tracker.add(new Item("itemA"));
+        Item item2 = tracker.add(new Item("itemB"));
+        Item item3 = tracker.add(new Item("itemC"));
+        Item item4 = tracker.add(new Item("itemA"));
+        Item item5 = tracker.add(new Item("itemC"));
         String name = "itemA";
         Input in = new StubInput(
                 new String[]{"0", name, "1"}
@@ -123,8 +123,8 @@ public class StartUITest {
                                         + "0. Find items by name" + System.lineSeparator()
                                         + "1. Exit" + System.lineSeparator()
                                         + "=== Find items by name ====" + System.lineSeparator()
-                                        + "Item{id=" + tracker.findAll()[0].getId() + ", name='" + tracker.findAll()[0].getName() + "\'}" + System.lineSeparator()
-                                        + "Item{id=" + tracker.findAll()[3].getId() + ", name='" + tracker.findAll()[3].getName() + "\'}" + System.lineSeparator()
+                                        + "Item{id=" + item1.getId() + ", name='" + item1.getName() + "\'}" + System.lineSeparator()
+                                        + "Item{id=" + item4.getId() + ", name='" + item4.getName() + "\'}" + System.lineSeparator()
                                         + "Menu." + System.lineSeparator()
                                         + "0. Find items by name" + System.lineSeparator()
                                         + "1. Exit" + System.lineSeparator()
@@ -140,8 +140,9 @@ public class StartUITest {
         tracker.add(new Item("itemC"));
         tracker.add(new Item("itemD"));
         tracker.add(new Item("itemE"));
+        int inputId = 4;
         Input in = new StubInput(
-                new String[]{"0", "4", "1"}
+                new String[]{"0", String.valueOf(inputId), "1"}
         );
         UserAction[] actions = {
                 new FindItemByIdAction(output),
