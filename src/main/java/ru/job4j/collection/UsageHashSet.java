b8c38c750d08499edf0b74a526b2e30a54578d46
11_pochta@bk.ru <11_pochta@bk.ru>
2022-08-03 17:49:39 +0600

    0. Set, HashSet, Iterator. [#10095 #328198]
    Добавил несколько дублирующих строк для изучения работы HashSet коллекций

--- src/main/java/ru/job4j/collection/UsageHashSet.java
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -6,9 +6,12 @@ public class UsageHashSet {
     public static void main(String[] args) {
         HashSet<String> autos = new HashSet<>();
         autos.add("Lada");
+        autos.add("Toyota");
         autos.add("BMW");
         autos.add("Volvo");
+        autos.add("BMW");
         autos.add("Toyota");
+        autos.add("Volvo");
 
         for (String auto : autos) {
             System.out.println(auto);
