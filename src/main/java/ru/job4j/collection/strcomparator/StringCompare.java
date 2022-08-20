845ced32dcc7453a8ac03c5db4560de09a6dc0bf
11_pochta@bk.ru <11_pochta@bk.ru>
2022-08-20 12:32:49 +0600

    3. Компаратор для строк [#35008 #334262]
    Переделал метод compare()

--- src/main/java/ru/job4j/collection/strcomparator/StringCompare.java
+++ src/main/java/ru/job4j/collection/strcomparator/StringCompare.java
@@ -5,18 +5,13 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        if (left.equals(right)) {
-            return 0;
-        }
-        if (left.length() <= right.length()) {
-            for (int i = 0; i < left.length(); i++) {
-                int compare = Character.compare(left.charAt(i), right.charAt(i));
-                if (compare == 0) {
-                    continue;
-                }
+        int lenMinStr = Math.min(left.length(), right.length());
+        for (int i = 0; i < lenMinStr; i++) {
+            int compare = Character.compare(left.charAt(i), right.charAt(i));
+            if (compare != 0) {
                 return compare;
             }
         }
-        return -1;
+        return Integer.compare(left.length(), right.length());
     }
 }
