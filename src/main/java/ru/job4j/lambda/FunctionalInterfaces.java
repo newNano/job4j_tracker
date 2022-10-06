ebb5aaf30fc47c2501534eecc48f6101bee4fc71
11_pochta@bk.ru <11_pochta@bk.ru>
2022-10-06 14:07:07 +0600

    2.2. Функциональный интерфейс [#247497 #349019] Изменил запись biPred и в условии if()

--- src/main/java/ru/job4j/lambda/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/lambda/FunctionalInterfaces.java
@@ -22,10 +22,11 @@ public class FunctionalInterfaces {
         biCon.accept(6, "six");
         biCon.accept(7, "seven");
 
-        BiPredicate<Integer, String> biPred = (i, s) -> i % 2 == 0 || map.get(i).length() == 4;
+        BiPredicate<Integer, String> biPred = (i, s) -> i % 2 == 0 || s.length() == 4;
         for (Integer i : map.keySet()) {
-            if (biPred.test(i, null)) {
-                System.out.println("key: " + i + " value: " + map.get(i));
+            String str = map.get(i);
+            if (biPred.test(i, str)) {
+                System.out.println("key: " + i + " value: " + str);
             }
         }
 
