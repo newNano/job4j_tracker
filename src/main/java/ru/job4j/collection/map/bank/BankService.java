55666fe5f8a86905f92de26cdcfe09c6c89d41b9
11_pochta@bk.ru <11_pochta@bk.ru>
2022-10-17 21:22:04 +0600

    7. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260]

--- src/main/java/ru/job4j/collection/map/bank/BankService.java
+++ src/main/java/ru/job4j/collection/map/bank/BankService.java
@@ -12,6 +12,7 @@ import java.util.Map;
  * 2. Удалять пользователя из системы.
  * 3. Добавлять пользователю банковский счет. У пользователя системы могут быть несколько счетов.
  * 4. Переводить деньги с одного банковского счета на другой счет.
+ *
  * @author NAZEKOV ULUKBEK
  * @version 1.0
  */
@@ -23,8 +24,9 @@ public class BankService {
 
     /**
      * Метод добавляет нового пользователя в систему.
+     *
      * @param user - пользователь, который добавляется в систему.
-     *              в случае его отсутствия в системе.
+     *             в случае его отсутствия в системе.
      */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<Account>());
@@ -32,10 +34,11 @@ public class BankService {
 
     /**
      * Метод добавляет новый счет к пользователю
+     *
      * @param passport - паспорт пользователя. По паспорту находим пользователя.
-     * После этого мы получим список всех счетов пользователя и
-     * если такого счета нет - добавим новый счет к ним.
-     * @param account - счет, который нужно добавить в список, если его нет.
+     *                 После этого мы получим список всех счетов пользователя и
+     *                 если такого счета нет - добавим новый счет к ним.
+     * @param account  - счет, который нужно добавить в список, если его нет.
      */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
@@ -49,52 +52,48 @@ public class BankService {
 
     /**
      * Метод ищет пользователя по номеру паспорта:
+     *
      * @param passport - паспорт пользователя.
      * @return - метод возвращает найденного по паспорту пользователя,
-     *          если пользовтеля с таким паспортом нет - возвращает null.
+     * если пользовтеля с таким паспортом нет - возвращает null.
      */
     public User findByPassport(String passport) {
-        User result = null;
-        for (User user : users.keySet()) {
-            if (user.getPassport().equals(passport)) {
-                result = user;
-                break;
-            }
-        }
-        return result;
+        return users.keySet()
+                .stream()
+                .filter(user -> user.getPassport().equals(passport))
+                .findFirst()
+                .orElse(null);
     }
 
     /**
      * Метод ищет счет пользователя по реквизитам.
-     * @param passport - паспорт пользователя
+     *
+     * @param passport  - паспорт пользователя
      * @param requisite - реквизиты пользователя
      * @return - метод возвращает счет, если есть пользовтель с такими данными,
-     *          в противном случае возвращает null.
+     * в противном случае возвращает null.
      */
     public Account findByRequisite(String passport, String requisite) {
-        Account result = null;
         User user = findByPassport(passport);
         if (user != null) {
-            List<Account> accounts = getAccounts(user);
-            for (Account account : accounts) {
-                if (account.getRequisite().equals(requisite)) {
-                    result = account;
-                    break;
-                }
-            }
+            return getAccounts(user).stream()
+                    .filter(account -> account.getRequisite().equals(requisite))
+                    .findFirst()
+                    .orElse(null);
         }
-        return result;
+        return null;
     }
 
     /**
      * Метод предназначен для перечисления денег с одного счёта на другой счёт.
-     * @param srcPassport - паспорт отправителя
-     * @param srcRequisite - реквизиты отправителя
-     * @param destPassport - паспорт получателя
+     *
+     * @param srcPassport   - паспорт отправителя
+     * @param srcRequisite  - реквизиты отправителя
+     * @param destPassport  - паспорт получателя
      * @param destRequisite - реквизиты получателя
-     * @param amount - сумма денег, которые нужно перевести
+     * @param amount        - сумма денег, которые нужно перевести
      * @return возвращает true если перевод прошел успешно,
-     *          в противном случае - false
+     * в противном случае - false
      */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
@@ -113,6 +112,7 @@ public class BankService {
 
     /**
      * Метод возвращает список всех счетов пользователя.
+     *
      * @param user - пользователь у которого получаем список всех его счетов.
      * @return возвращает список всех счетов пользователя.
      */
