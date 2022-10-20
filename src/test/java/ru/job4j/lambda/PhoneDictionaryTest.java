31329dbf9ac48dd3b900d7ba348d17acca3bf967
11_pochta@bk.ru <11_pochta@bk.ru>
2022-10-20 14:11:41 +0600

    2. Local-Variable Type Inference [#70626 #352908]
    Замена на var

--- src/test/java/ru/job4j/lambda/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/lambda/PhoneDictionaryTest.java
@@ -1,27 +1,26 @@
 package ru.job4j.lambda;
 
-import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class PhoneDictionaryTest {
     @Test
     public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Petr");
+        var persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
     }
 
     @Test
     public void whenNotFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Vasya");
+        var persons = phones.find("Vasya");
         assertThat(persons.size()).isEqualTo(0);
     }
 }
