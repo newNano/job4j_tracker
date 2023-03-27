e96349ce96e07f7ac4d615ed90d2500fd9f29db3
nano <11_pochta@bk.ru>
2023-03-27 13:19:18 +0600

    5. PriorityQueue [#504944 #404359] Добавил 3 теста для проверки работы комбинированного компаратора

--- src/test/java/ru/job4j/collection/queue/priority/ScheduleTaskTest.java
+++ src/test/java/ru/job4j/collection/queue/priority/ScheduleTaskTest.java
@@ -1,8 +1,10 @@
 package ru.job4j.collection.queue.priority;
 
 import org.junit.jupiter.api.Test;
+import java.util.Arrays;
 import java.util.Comparator;
-import static org.assertj.core.api.Assertions.*;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
 import static ru.job4j.collection.queue.priority.Position.*;
 
 class ScheduleTaskTest {
@@ -35,5 +37,66 @@ class ScheduleTaskTest {
         expected = "description_2";
         result = scheduleTask.getTask().description();
         assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenComparatorTaskByPositionAscAndByUrgencyDesc() {
+        Comparator<Task> comparator = new TaskByPositionAsc()
+                                                .thenComparing(new TaskByUrgencyDesc());
+        ScheduleTask scheduleTask = new ScheduleTask(comparator);
+        scheduleTask.addTask(new Task(DEPARTMENT_HEAD, "description_1", 10));
+        scheduleTask.addTask(new Task(DIRECTOR, "description_2", 4));
+        scheduleTask.addTask(new Task(DIRECTOR, "description_3", 8));
+        List<Task> list = Arrays.asList(
+            scheduleTask.getTask(),
+            scheduleTask.getTask(),
+            scheduleTask.getTask()
+        );
+        List<Task> expected = Arrays.asList(
+            new Task(DIRECTOR, "description_3", 8),
+            new Task(DIRECTOR, "description_2", 4),
+            new Task(DEPARTMENT_HEAD, "description_1", 10)
+        );
+        assertThat(list).isEqualTo(expected);
+    }
+
+    @Test
+    void whenTwoDirectorTaskThenDepartmentHeadTask() {
+        Comparator<Task> comparator = new TaskByPositionAsc()
+                .thenComparing(new TaskByUrgencyDesc());
+        ScheduleTask scheduleTask = new ScheduleTask(comparator);
+        scheduleTask.addTask(new Task(DEPARTMENT_HEAD, "description_1", 10));
+        scheduleTask.addTask(new Task(DIRECTOR, "description_2", 4));
+        scheduleTask.addTask(new Task(DIRECTOR, "description_3", 8));
+        String expected1 = "description_3";
+        String result1 = scheduleTask.getTask().description();
+        assertThat(result1).isEqualTo(expected1);
+        String expected2 = "description_2";
+        String result2 = scheduleTask.getTask().description();
+        assertThat(result2).isEqualTo(expected2);
+        String expected3 = "description_1";
+        String result3 = scheduleTask.getTask().description();
+        assertThat(result3).isEqualTo(expected3);
+    }
+
+    @Test
+    void whenTwoDepartmentHeadTaskThenManagerTask() {
+        Comparator<Task> comparator = new TaskByPositionAsc()
+                .thenComparing(new TaskByUrgencyDesc());
+        ScheduleTask scheduleTask = new ScheduleTask(comparator);
+        scheduleTask.addTask(new Task(DEPARTMENT_HEAD, "description_1", 7));
+        scheduleTask.addTask(new Task(MANAGER, "description_2", 10));
+        scheduleTask.addTask(new Task(DEPARTMENT_HEAD, "description_3", 7));
+        List<Task> list = Arrays.asList(
+            scheduleTask.getTask(),
+            scheduleTask.getTask(),
+            scheduleTask.getTask()
+        );
+        List<Task> expected = Arrays.asList(
+            new Task(DEPARTMENT_HEAD, "description_1", 7),
+            new Task(DEPARTMENT_HEAD, "description_3", 7),
+            new Task(MANAGER, "description_2", 10)
+        );
+        assertThat(list).isEqualTo(expected);
     }
 }
